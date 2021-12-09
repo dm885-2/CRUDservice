66d49d636541f1ef23d05d023eb0dff58ac4c3e4
= <=>
2021-12-09 20:38:43 +0100

    done for now

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import {host,
         subscriber
         } from "./helpers.js";
 
-let createTable = `create table if not exists files(
-                    id int primary key auto_increment,
+let createTable = `CREATE TABLE if not exists files(
                     filename varchar(50) not null,
                     data text, 
-                    userid int)`;
+                    userid int,
+                    filetype varchar(5) not null,
+                    PRIMARY KEY (userid, filename))`;
 
 connection.query(createTable, (error, results) => {
     if(error) {
@@ -28,18 +29,19 @@ if(process.env.RAPID)
                 let filename = msg.filename;
                 let data = msg.data;
                 let userId = msg.userId;
+                let filetype = msg.filetype;
 
-                let query = `INSERT INTO files(filename,data,userid)
-                             VALUES(?,?,?)`;
+                let query = `INSERT INTO files(filename,data,userid,filetype)
+                             VALUES(?,?,?,?)`;
                 
-                let params = [filename, data, userId];
+                let params = [filename, data, userId, filetype];
 
                 connection.query(query, params, (error, results) => {
                     if (error) {
                         publish('create-file-response', {error: true, message: error});
                         return;
                     }
-                    publish('create-file-response', {error: false, message: "File created successfully."})
+                    publish('create-file-response', {error: false, message: "File created successfully.", filename: filename, filetype: filetype, userId: userId})
                 });                              
             }
         },
@@ -51,14 +53,14 @@ if(process.env.RAPID)
                 let userId = msg.userId;
                 let filename = msg.filename;
 
-                let query = 'SELECT * FROM files WHERE userId=? AND filename=?';
+                let query = 'SELECT * FROM files WHERE userid=? AND filename=?';
 
                 connection.query(query, [userId, filename], (error, results) => {
                     if(error || Object.keys(results).length === 0) {
                         publish('read-file-response', {error: true, message: "File not found", errormessage: error});
                         return;
                     }
-                    publish('read-file-response', {error: false, filename: results[0].filename, data: results[0].data, userid: results[0].userid})
+                    publish('read-file-response', {error: false, filename: results[0].filename, filetype: results[0].filetype, data: results[0].data, userid: results[0].userid})
                 });
             }
         },
@@ -66,21 +68,22 @@ if(process.env.RAPID)
             river: "CRUD",
             event: "update-file",
             work: (msg, publish) => {
-                let fileId = msg.fileId.toString();
-                let data = msg.data.toString();
+                let userId = msg.userId;
+                let filename = msg.filename;
+                let data = msg.data;
 
                 let query = `UPDATE files
                              SET data = ?
-                             WHERE id = ?`;
-                let params = [data, fileId];
+                             WHERE userid = ? AND filename = ?`;
+                let params = [data, userId, filename];
 
                 connection.query(query, params, (error, results) => {
                     if(error) {
-                        publish('update-file-response', error.message);
+                        publish('update-file-response', {error: true, message: "File not found", errormessage: error});
                         return;
                     }
 
-                    publish('update-file-response', `File with id ${results.insertId} updated succesfully.`)
+                    publish('update-file-response', {error: false, message: "File updated successfully", filename: filename, filetype: filetype, userId: userId})
                 });            
             }
         },
@@ -88,17 +91,18 @@ if(process.env.RAPID)
             river: "CRUD",
             event: "delete-file",
             work: (msg, publish) => {
-                let fileId = msg.fileId.toString();
+                let userId = msg.userId;
+                let filename = msg.filename;
                 
-                let query = `DELETE FROM files WHERE id = ?`
+                let query = `DELETE FROM files WHERE userid = ? AND filename = ?`
 
-                connection.query(query, fileId, (error, results) => {
+                connection.query(query, [userId, filename], (error, results) => {
                     if(error) {
-                        publish('delete-file-response', error.message);
+                        publish('delete-file-response', {error: true, message: error.message});
                         return;
                     }
 
-                    publish('delete-file-response', `File with id ${results,insertId} deleted succesfully.`)
+                    publish('delete-file-response', {error: false, message: "File deleted succesfully", filename: filename, filetype: filetype, userId: userId})
                 });
             }
         },
@@ -106,17 +110,16 @@ if(process.env.RAPID)
             river: "CRUD",
             event: "get-all-files",
             work: (msg, publish) => {
-                let userId = msg.userId.toString();
+                let userId = msg.userId;
+                let filetype = msg.filetype;
+                let query = 'SELECT * FROM files WHERE userid=? AND filetype = ?';
 
-                let query = 'SELECT * FROM files where userid=?'
-
-                connection.query(query, userId, (error, results) => {
+                connection.query(query, [userId, filetype], (error, results) => {
                     if(error) {
-                        publish('get-all-files-response', error.message);
+                        publish('get-all-files-response', {error: true, message: error});
                         return;
                     }
-                    let payload = JSON.stringify(results);
-                    publish('get-all-files-response', payload);
+                    publish('get-all-files-response', {error: false, files: results});
                 });                
             }
         },
