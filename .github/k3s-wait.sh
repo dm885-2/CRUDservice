6a2379715408da436f93278bf3563f8334eb62ab
= <=>
2021-12-10 21:18:24 +0100

    wait test

--- .github/k3s-wait.sh
+++ .github/k3s-wait.sh
@@ -4,7 +4,5 @@ tmp2=$(kubectl get pods 2>/dev/null |grep '0/1')
 while [ ! -z "$tmp" ] && [ ! -z "$tmp2" ]; do
     tmp=$(kubectl -n rabbits get pods 2>/dev/null |grep '0/1')
     tmp2=$(kubectl get pods 2>/dev/null |grep '0/1')
-    echo $(kubectl -n rabbits get pods)
-    echo $(kubectl get pods)
     sleep 10
 done
