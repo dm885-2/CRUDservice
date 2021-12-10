0c673813d1aa7b3f2b0c6d35f72062b51f5a3e3d
= <=>
2021-12-10 20:17:22 +0100

    started pipeline

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,4 +1,4 @@
-const ip = "http://127.0.0.1:3000"; // Should be url to UI
+const ip = "http://127.0.0.1:8080"; // Should be url to UI
 
 describe('Rapid River Tests', () => {
     it("Test GET /", () => {  
@@ -6,4 +6,63 @@ describe('Rapid River Tests', () => {
             cy.wrap(response).its('status').should('eq', 200);
         })
     });
-  })
+
+    it("CREATE file test", () => {
+        cy.request('POST', ip + "/files/create", {
+            filename: "testfile.mzn",
+            filetype: "mzn",
+            data: "This is the file content!"
+        }).then((response) => {
+            cy.wrap(response).its('error').should('eq', false);
+        })
+    });
+
+    it("READ file test", () => {
+        cy.request('GET', ip + "/files/read", {
+            filename: "testfile.mzn",
+            filetype: "mzn"
+        }).then((response) => {
+            cy.wrap(response).its('error').should('eq', false);
+        })
+    })
+
+    it("UPDATE file test", () => {
+        cy.request('PUT', ip + "/files/update", {
+            filename: "testfile.mzn",
+            data: "new content of the file!",
+            filetype: "mzn"
+        }).then((response) => {
+            cy.wrap(response).its('error').should('eq', false);
+        })
+    })
+
+    it("CREATE some more files", () => {
+        for(var i = 0; i < 5; i++) {
+            cy.request('POST', ip + "/files/create", {
+                filename: "testfile" + i + ".mzn",
+                data: "new content of the file" + i + "!",
+                filetype: "mzn"
+            }).then((response) => {
+                cy.wrap(response).its('error').should('eq', false);
+            })
+        }
+    })
+
+    it("DELETE file test", () => {
+        cy.request('DELETE', ip + "/files/delete", {
+            filename: "testfile.mzn",
+            filetype: "mzn"
+            
+        }).then((response) => {
+            cy.wrap(response).its('error').should('eq', false);
+        })
+    })
+
+    it("GET ALL file test", () => {
+        cy.request('GET', ip + "/files/getall", {
+            filetype: "mzn"
+        }).then((response) => {
+            cy.wrap(response).its('error').should('eq', false);
+        })
+    })
+})
