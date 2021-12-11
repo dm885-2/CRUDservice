995c40a7c3e4be5f0699b64dbe4b367d6ac05140
= <=>
2021-12-11 21:37:49 +0100

    cypress

--- .github/k3s-wait.sh
+++ .github/k3s-wait.sh
@@ -1,6 +1,6 @@
 sleep 10
 flag=0
-while [ flag == 0 ]; do
+while [ $flag == 0 ]; do
     tmp=$(kubectl -n rabbits get pods 2>/dev/null |grep '0/1')
     tmp2=$(kubectl get pods 2>/dev/null |grep '0/1')
 
