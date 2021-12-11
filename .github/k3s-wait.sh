79b6936764b59c0fc05476b0685fefdddd8d0e4e
= <=>
2021-12-11 21:42:52 +0100

    cypress

--- .github/k3s-wait.sh
+++ .github/k3s-wait.sh
@@ -1,12 +1,8 @@
 sleep 10
-flag=0
-while [ $flag == 0 ]; do
+tmp=$(kubectl -n rabbits get pods 2>/dev/null |grep '0/1')
+tmp2=$(kubectl get pods 2>/dev/null |grep '0/1')
+while [ ! -z "$tmp" ] && [ ! -z "$tmp2" ]; do
     tmp=$(kubectl -n rabbits get pods 2>/dev/null |grep '0/1')
     tmp2=$(kubectl get pods 2>/dev/null |grep '0/1')
-
-    if [ -z "$tmp" ] && [ -z "$tmp2" ]; then 
-        flag = 1
-    fi 
-
     sleep 10
 done
