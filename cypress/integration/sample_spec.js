60ced9e9db8089ddb520657c2a65beeca06c748a
= <=>
2021-12-12 15:47:27 +0100

    cypress

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -3,7 +3,7 @@ describe('CRUD Test', () => {
     //add a files before each test
     beforeEach(()=> {
         Cypress.Cookies.defaults({
-            preserve: "session_id"
+            preserve: "sessionId"
           })
 
         cy.request("POST", "/files", {
@@ -11,7 +11,7 @@ describe('CRUD Test', () => {
             "filetype": "mzn",
             "data": "This is the file content!"
         }).then((res) => {
-            return;
+            console.log(res.body.sessionId);
         });
     })
     
@@ -23,7 +23,7 @@ describe('CRUD Test', () => {
             res.body.results.forEach(file => {
                 cy.request('DELETE', "/files/"+file.fileId);
             })
-            return;
+            console.log(res.body.sessionId);
         });
    })
 
