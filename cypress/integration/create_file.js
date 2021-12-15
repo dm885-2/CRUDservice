0a5604b22c9d6e22ac9940e8f4e1fe5bb9e5691c
= <=>
2021-12-15 23:01:05 +0100

    new commands

--- cypress/integration/create_file.js
+++ cypress/integration/create_file.js
@@ -1,26 +1,54 @@
-// describe('CREATE Test', () => {
+describe('CREATE Test', () => {
 
-//     beforeEach(()=> {
-//         const uname = "u"+Date.now();
-//         const pass = "p"+Date.now();
-//         cy.register(uname, pass);
-//         cy.login(uname, pass);
-//         cy.getAT();
-//     });
+    beforeEach(()=> {
+        const uname = "u"+Date.now();
+        const pass = "p"+Date.now();
+        cy.register(uname, pass);
+        cy.login(uname, pass);
+        cy.getAT();
+    });
 
-//     it("CREATE test", () => {
-//         cy.request({
-//             method: "POST",
-//             url: "/files",
-//             headers: {
-//                 "Content-Type": "application/json",
-//                 "Authorization": "Bearer " + Cypress.env("token")
-//             },
-//             body: {
-//                 filename: "testfile.mzn",
-//                 filetype: "mzn",
-//                 data : "fuk of ya buggar"
-//             }
-//         })
-//     })
-// });
+    it("CREATE test", () => { 
+        cy.request({
+            method: "POST",
+            url: "/files",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + Cypress.env("token")
+            },
+            body: {
+                filename: "testFile.mzn",
+                filetype: "mzn",
+                data : "fuk of ya buggar"
+            }
+        }).then(response => {
+            expect(response.status).to.eq(200);
+            expect(response.body.error).to.eq(false);
+            expect(response.body.filename).to.eq('testFile.mzn');
+            expect(response.body.filetype).to.eq('mzn');
+        })
+    })
+
+    afterEach(() => {
+        cy.request({
+            method: "GET",
+            url: "/files/all/mzn",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + Cypress.env("token") 
+            }
+        }).then(res => {
+            res.body.results.forEach(file => {
+                cy.request({
+                    method: "DELETE",
+                    url: "/files/"+file.fileId,
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": "Bearer " + Cypress.env("token") 
+                    }
+                })
+                return;
+            });
+        })
+    })
+});
--- cypress/integration/read_spec.js
+++ cypress/integration/read_spec.js
@@ -1,103 +1,103 @@
 
-describe('READ Test', () => {
+// describe('READ Test', () => {
 
-    var password = "ogi";
+//     var password = "ogi";
     
-    var currentdate = new Date(); 
-    var datetime = "" + currentdate.getDate() + (currentdate.getMonth()+1) + currentdate.getFullYear()
-    + currentdate.getHours()  
-    + currentdate.getMinutes()
-    + currentdate.getSeconds();
+//     var currentdate = new Date(); 
+//     var datetime = "" + currentdate.getDate() + (currentdate.getMonth()+1) + currentdate.getFullYear()
+//     + currentdate.getHours()  
+//     + currentdate.getMinutes()
+//     + currentdate.getSeconds();
     
-    var at;
-    //Creates a user before spec file is run.
-    before(()=> {
-        Cypress.Cookies.defaults({
-            preserve: "sessionId"
-        })
+//     var at;
+//     //Creates a user before spec file is run.
+//     before(()=> {
+//         Cypress.Cookies.defaults({
+//             preserve: "sessionId"
+//         })
         
-        let username = datetime;
-        var rt
-        cy.request("POST", "/auth/register", {
-            username: username,
-            password: password,
-            passwordRepeat: password
-        }).then(res => {
-            cy.request("POST", "/auth/login", {
-                username: username,
-                password: password
-            }).then(res2 => {
-                rt = res2.body.refreshToken;
-            }); 
-        });   
+//         let username = datetime;
+//         var rt
+//         cy.request("POST", "/auth/register", {
+//             username: username,
+//             password: password,
+//             passwordRepeat: password
+//         }).then(res => {
+//             cy.request("POST", "/auth/login", {
+//                 username: username,
+//                 password: password
+//             }).then(res2 => {
+//                 rt = res2.body.refreshToken;
+//             }); 
+//         });   
 
-        cy.request("POST", "/auth/accessToken", {
-            refreshToken : rt
-        }).then(res3 => {
-            at = res3.body.accessToken;
-            //add a files before the test
-            cy.request({
-                method: "POST",
-                url: "/files",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${at}`
-                },
-                body:  {
-                    "filename": "testwFile.mzn",
-                    "filetype": "mzn",
-                    "data": "This is the file content!"
-                }
+//         cy.request("POST", "/auth/accessToken", {
+//             refreshToken : rt
+//         }).then(res3 => {
+//             at = res3.body.accessToken;
+//             //add a files before the test
+//             cy.request({
+//                 method: "POST",
+//                 url: "/files",
+//                 headers: {
+//                     "Content-Type": "application/json",
+//                     "Authorization": `Bearer ${at}`
+//                 },
+//                 body:  {
+//                     "filename": "testwFile.mzn",
+//                     "filetype": "mzn",
+//                     "data": "This is the file content!"
+//                 }
                 
-            }).then(l => {return})
-        })
-    })
+//             }).then(l => {return})
+//         })
+//     })
     
-    it("READ TEST", () => {
-        cy.request({
-            method: "GET",
-            url: "/files/all/mzn",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${at}`
-            }
-        }).then(file => {
-            cy.request({
-                method: "GET",
-                url: "/files/"+file.body.results[0].fileId,
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${at}`
-                }
-            }).then((res) => {
-                expect(res).to.have.property("status", 200);
-                expect(res.body).to.have.property("error", false);
-                return;
-            });
-        });
-    });
+//     it("READ TEST", () => {
+//         cy.request({
+//             method: "GET",
+//             url: "/files/all/mzn",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Authorization": `Bearer ${at}`
+//             }
+//         }).then(file => {
+//             cy.request({
+//                 method: "GET",
+//                 url: "/files/"+file.body.results[0].fileId,
+//                 headers: {
+//                     "Content-Type": "application/json",
+//                     "Authorization": `Bearer ${at}`
+//                 }
+//             }).then((res) => {
+//                 expect(res).to.have.property("status", 200);
+//                 expect(res.body).to.have.property("error", false);
+//                 return;
+//             });
+//         });
+//     });
 
-    //Delete all files after each test
-   after(()=>{
-        cy.request({
-            method: "GET",
-            url: "/files/all/mzn",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${at}`
-            }
-        }).then(res => {
-            res.body.results.forEach(file => {
-                cy.request({
-                    method: "DELETE",
-                    url: "/files/"+file.fileId,
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Authorization": `Bearer ${at}`
-                    }
-                })
-                return;
-            });
-        })
-    })
-});
+//     //Delete all files after each test
+//    after(()=>{
+//         cy.request({
+//             method: "GET",
+//             url: "/files/all/mzn",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Authorization": `Bearer ${at}`
+//             }
+//         }).then(res => {
+//             res.body.results.forEach(file => {
+//                 cy.request({
+//                     method: "DELETE",
+//                     url: "/files/"+file.fileId,
+//                     headers: {
+//                         "Content-Type": "application/json",
+//                         "Authorization": `Bearer ${at}`
+//                     }
+//                 })
+//                 return;
+//             });
+//         })
+//     })
+// });
